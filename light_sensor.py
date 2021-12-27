2114417124c6d3d959c16aafdcb88945693be17a
Rodney Peck <fogcat5@gmail.com>
2021-12-26 19:07:25 -0800

    added simple light sensor code

---
+++ light_sensor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import board
+import digitalio
+import time
+
+l = digitalio.DigitalInOut(board.D26)
+while True:
+  l.direction=digitalio.Direction.OUTPUT
+  l.value=False
+  time.sleep(1)
+  l.direction=digitalio.Direction.INPUT
+  t=time.time()
+  while not l.value:
+    time.sleep(0.1)
+  d = time.time()-t
+  v = 'off'
+  if d < 0.3:
+      v = 'on'
+  print(v, l.value, time.time()-t)
+  
