73f1aae046443872351eb0d32d74ffc88db350d8
Rodney Peck <fogcat5@gmail.com>
2021-12-26 19:22:19 -0800

    faster testing light

--- light_sensor.py
+++ light_sensor.py
@@ -7,11 +7,11 @@ l = digitalio.DigitalInOut(board.D26)
 while True:
   l.direction=digitalio.Direction.OUTPUT
   l.value=False
-  time.sleep(1)
+  time.sleep(0.1)
   l.direction=digitalio.Direction.INPUT
   t=time.time()
   while not l.value:
-    time.sleep(0.1)
+    time.sleep(0.001)
   d = time.time()-t
   v = 'off'
   if d < 0.3:
