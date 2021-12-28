5a5da8316b071c9fc744531506c9e768c9fe81cb
Rodney Peck <fogcat5@gmail.com>
2021-12-28 12:13:39 -0800

    added create_table sql

---
+++ create_table.sql
@@ -0,0 +1,3 @@
+  CREATE TABLE weather (epoch INTEGER, solarRadiation FLOAT, uv FLOAT,
+          winddir FLOAT, humidity FLOAT, temp FLOAT, windSpeed FLOAT,
+          windGust FLOAT, pressure FLOAT, precipRate FLOAT, precipTotal FLOAT);
