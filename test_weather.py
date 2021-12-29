27179cd15ba795db7f21cfd6d116fa582112fcdb
Rodney Peck <ropeck@google.com>
2021-12-28 16:51:00 -0800

    database mock for testing

--- test_weather.py
+++ test_weather.py
@@ -9,15 +9,23 @@ class TestWeather(TestCase):
         self.mock_sql = p.start()
         self.addCleanup(p.stop)
 
+        p = patch('requests.get')
+        self.mock_get = p.start()
+        self.addCleanup(p.stop)
+
 # TODO: mock station data API call
 # TODO: mock forecast data API call
+# TODO: only update database if new data
+    # TODO: collect station data for 5minute and hourly updates and save to databases
+    # TODO: add testing for all that too
 
     def test_update_database(self):
         lat = "36.9"
         lon = "-121.9"
-        api_key_weather = "b69ed8db8927bd983bf8388c067c5626"
+        api_key_weather = "apikey"
+        self.mock_get.json.return_value = None
         w = weather.Weather(lat, lon, api_key_weather)
-        w.update_database()
+        w.update()
         ex = self.mock_sql.connect().cursor().execute
         self.assertTrue(ex.called)
 
