a34c0bde0add2557ac98b2a9e1d3349415b032c1
Rodney Peck <ropeck@google.com>
2021-12-28 16:56:25 -0800

    draw the name when an icon is missing

--- display.py
+++ display.py
@@ -59,4 +59,4 @@ class Display:
             self.im_black.paste(im_icon, (x, y), im_icon)
         except Exception as e:
             print(str(e))
-            return
+            self.im_black.text((x, y), "not found: {}".format(icon), fill="red", font=font48)
