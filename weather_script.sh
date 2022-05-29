ce9bb0445376edea5371110f49a13f664f2f0843
Rodney Peck <fogcat5@gmail.com>
2022-05-29 13:19:37 -0700

    fixed api key and script on reboot

--- weather_script.sh
+++ weather_script.sh
@@ -1,14 +1,17 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # set to save API response data from wunderground
 #export WEATHER_API_RESPONSE_PATH=/home/pi/weather/data
 #export WEATHER_DEBUG=True
-export WEATHER_WUNDERGROUND_API_KEY=5bb5ecb88c674ef9b5ecb88c67def9fb
+# new key 20220529
+# https://www.wunderground.com/member/api-keys
+export WEATHER_WUNDERGROUND_API_KEY=9f5af8c970ac408e9af8c970ac008e14
 export WEATHER_STATION_ID=KCAAPTOS92
 export WEATHER_LAT=36.9
 export WEATHER_LONG=-121.9
 export WEATHER_FORECAST_API_KEY=b69ed8db8927bd983bf8388c067c5626
 export WEATHER_NEWS_API_KEY=72064d803ca1466ca192b1031038cbbc
 
+cd /home/pi/weather
 ./test_weather.py && ./weather_station.py
 
