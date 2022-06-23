c282ff9ae9f32d87a16f9098b2b3adf61c667390
Rodney Peck <fogcat5@gmail.com>
2022-06-23 10:51:22 -0700

    update env key script; add 25 cloudy icon

--- weather_script.sh
+++ weather_script.sh
@@ -2,15 +2,9 @@
 
 # set to save API response data from wunderground
 #export WEATHER_API_RESPONSE_PATH=/home/pi/weather/data
-#export WEATHER_DEBUG=True
-# new key 20220529
-# https://www.wunderground.com/member/api-keys
-export WEATHER_WUNDERGROUND_API_KEY=9f5af8c970ac408e9af8c970ac008e14
-export WEATHER_STATION_ID=KCAAPTOS92
-export WEATHER_LAT=36.9
-export WEATHER_LONG=-121.9
-export WEATHER_FORECAST_API_KEY=b69ed8db8927bd983bf8388c067c5626
-export WEATHER_NEWS_API_KEY=72064d803ca1466ca192b1031038cbbc
+export WEATHER_DEBUG=True
+
+source /home/pi/.weather_keys
 
 cd /home/pi/weather
 ./test_weather.py && ./weather_station.py
